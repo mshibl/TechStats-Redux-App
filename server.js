58c288da8c3e1224075f382fe82c274a9b0d0e0d
mshibl <mohamed.shibl@hotmail.com>
2016-09-03 15:48:36 -0700

    debug prod deployment

--- server.js
+++ server.js
@@ -9,31 +9,26 @@ const compiler = webpack(config);
 const port = isProduction ? process.env.PORT : 3000;
 
 if(!isProduction){
-	console.log("here")
-	app.use(require('webpack-dev-middleware')(compiler, {
-	  noInfo: true,
-	  publicPath: config.output.publicPath
-	}));
+	// console.log("here")
+	// app.use(require('webpack-dev-middleware')(compiler, {
+	//   noInfo: true,
+	//   publicPath: config.output.publicPath
+	// }));
 	
-	app.use(require('webpack-hot-middleware')(compiler));
-	// console.log(config.output.publicPath)
-	// app.use(express.static(__dirname));
-} else {
-	// const child_process = require('child_process');
-	// child_process.exec("webpack -p --config webpack.config.prod", function (error, stdout, stderr) {
-	// 	console.log('stdout: ' + stdout);
-	// 	console.log('stderr: ' + stderr);
-	// 	if (error) { console.log('exec error: ' + error); }
-	// });
+	// app.use(require('webpack-hot-middleware')(compiler));
 	console.log(path.join(__dirname, 'index.html'))
 	console.log(__dirname + '/index.html')
 	app.use(express.static(__dirname));
+} else {
+	// console.log(path.join(__dirname, 'index.html'))
+	// console.log(__dirname + '/index.html')
+	// app.use(express.static(__dirname));
 }
 
-app.get('*', (req, res, err) => {
-	console.log(err)
-  res.sendFile('index.html' + {root: __dirname})
-});
+app.get('*', function(req, res) {
+	console.log("requesting index.html")
+  res.sendFile(__dirname + '/index.html')
+})
 
 app.listen(port, 'localhost', (err) => {
   if (err) {
